22cb358bc6ab6d581b11d4e49af0f64c29829e3a
soupji <>
2024-12-31 15:27:51 +0100

    jupyter notebook, choix requete api ou ajout dans mongodb

--- main.py
+++ main.py
@@ -37,11 +37,12 @@ collection = db["villes"]
 def save_to_mongodb(city_data):
     collection.insert_one(city_data)
 
-french_cities = get_french_city_ids()
+def run_mongo():
+    french_cities = get_french_city_ids()
 
-for city_id in french_cities:
-    weather_data = get_weather_data(city_id)
-    save_to_mongodb(weather_data)
+    for city_id in french_cities:
+        weather_data = get_weather_data(city_id)
+        save_to_mongodb(weather_data)
 
 @app.route('/')
 def home():
@@ -53,5 +54,12 @@ def get_city_weather(city_id):
     
     return jsonify(weather_data)
 
+run = input("Souhaitez-vous interroger l'API ou sauvegarder les données dans MongoDB? (y/n): ")
+
+if run == 'y':
+    run_mongo()
+else:
+    pass
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run( )
